e493a641ffbe3318e5ead0b6d1bae377c23ad4a7
Steven Yu <sky3947@rit.edu>
2022-02-09 15:30:27 -0500

    Mock games and initial Games component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/mocks/mock-games.ts
@@ -0,0 +1,8 @@
+import { Game } from "src/game";
+
+export const GAMES: Game[] = [
+  {id: 0, name: "PAC-MAN", cost: 1},
+  {id: 1, name: "Mario Bros", cost: 2},
+  {id: 2, name: "Space Invaders", cost: 3},
+  {id: 3, name: "Sonic Adventure Game", cost: 4},
+]
