288d8e5b1345d5fd0ee99cdc036b457ac8f9e243
Steven Yu <sky3947@rit.edu>
2022-02-10 14:28:42 -0500

    Styling

--- src/mocks/mock-games.ts
+++ src/mocks/mock-games.ts
@@ -2,7 +2,11 @@ import { Game } from "src/game";
 
 export const GAMES: Game[] = [
   {id: 0, name: "PAC-MAN", cost: 1},
-  {id: 1, name: "Mario Bros", cost: 2},
-  {id: 2, name: "Space Invaders", cost: 3},
-  {id: 3, name: "Sonic Adventure", cost: 4},
+  {id: 1, name: "Asteroids", cost: 1},
+  {id: 2, name: "Mario Bros", cost: 2},
+  {id: 3, name: "Donkey Kong", cost: 2},
+  {id: 4, name: "Dig Dug", cost: 2},
+  {id: 5, name: "Space Invaders", cost: 3},
+  {id: 6, name: "Galaga", cost: 3},
+  {id: 7, name: "Sonic Adventure", cost: 4},
 ]
